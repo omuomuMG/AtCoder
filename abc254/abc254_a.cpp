4533e9f6f24c5d7227ea6fb10bc13a60355dc60a
omuomuMG <omuomunotrice@gmail.com>
2024-04-15 14:50:49 +0000

    Added my codes from AtCoder

---
+++ abc254/abc254_a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define all(v) v.begin(), v.end()
+#define rep(i, n) for (int i = 0; i < n; i++)
+using ll = long long;
+ 
+int main() {
+  int n;
+  cin>>n;
+  ll temp = n/10;
+  cout <<temp%10<<n%10;
+}
